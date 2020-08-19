ca209a8568b73760b0f14002a89b62be4df74b21
rabinkumarpati <rabinkumarpati@gmail.com>
2020-08-19 14:56:52 +0530

    Added my project

---
+++ app/routes/contact.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+export default class ContactRoute extends Route {
+    async model(){
+
+         let response = await fetch("http://localhost:4600/api/offices");
+        let data = await response.json();
+        return data;
+    }
+}
